298fcbaad8436f2f6b5b2f243f896d922c6f662f
netteyr14 <753951852456arvin@gmail.com>
2026-04-28 17:07:49 +0800

    [UPDATE] Env to Hardcoded again. For easier local runs

--- config/db.php
+++ config/db.php
@@ -18,6 +18,9 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+
+    // SSL for Aiven
+    PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
 ];
 
 // Add SSL for Aiven if provided
